781d18065c2ef6e9cfdc1a63a79dcc43378eb6ed
Harshit <47531747+Fumenoid@users.noreply.github.com>
2019-02-15 01:12:53 +0530

    Comments added.

--- circle.c
+++ circle.c
@@ -1,12 +1,22 @@
 #include<stdio.h>
-#define PI 3.14
+#define PI 3.14  //This a macro which later gets substituted into the code....in preprocessing stage
+
 void main()
 {
-float radius, area, circumference;
-printf("Enter the radius of circle \n");
-scanf("%f", &radius);
-area=(PI*radius*radius);
-printf("The area of the circle is %0.2f \n", area);
-circumference=(2*PI*radius);
-printf("The circumference of the circle is %0.2f \n", circumference);
+  //This is an easy code in C to calculate the area and circumference of the circle.
+  
+float radius, area, circumference;   //variable
+  
+printf("Enter the radius of circle \n"); 
+  
+scanf("%f", &radius);  // Getting the radius from the user.
+  
+area=(PI*radius*radius); // calculating area.
+  
+printf("The area of the circle is %0.2f \n", area); //printing the area with precission 2.. like 36.21
+  
+circumference=(2*PI*radius);  //Calculating circumference.
+  
+printf("The circumference of the circle is %0.2f \n", circumference); //printing it with precission 2.
+  
 }
